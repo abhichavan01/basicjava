5f3f957ab050e8d31c05d924262e6c24a99aa87e
abhichavan01 <abhishek.chavan@apaarinfosystems.com>
2023-01-23 13:06:37 +0530

    Looping assignment

--- src/basics/looping/OddNumAestrikPyramid.java
+++ src/basics/looping/OddNumAestrikPyramid.java
@@ -10,19 +10,21 @@ public class OddNumAestrikPyramid {
                 Scanner sc = new Scanner(System.in);
                 System.out.print("Enter the value of n: ");
                 n = sc.nextInt();
-                for (row = 0; row < n; row++) {
+                for (row = 0; row <= n; row++) {
 
-                    for (space = 0; space <=  n - row; space++) {
+                    for (space = 0; space <= n - row; space++) {
 
                         System.out.print(" ");
                     }
 
 
                     for (col = 1; col <= row; col++) {
-
-                        System.out.print(" "+row);
+                        if ((row % 2) == 0) {
+                            System.out.print(" ");
+                        } else {
+                            System.out.print(" *");
+                        }
                     }
-
                     System.out.println();
                 }
             }
